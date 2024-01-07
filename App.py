a45464de21d18f43f61a8fcdfb5e96faaa5fdb97
deekshithreddy12 <deekshithch66@gmail.com>
2024-01-07 00:44:24 -0600

    last uptaded on saturday

--- App.py
+++ App.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask , render_template
 app = Flask(__name__)
+if __name__=="__main__":
+    with app.app_context():
+        app.run(debug=True)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! Im divsss'
+    return render_template('index.html')
 
+
+@app.route('/about')
+def about():
+    return 'This is a about page'
