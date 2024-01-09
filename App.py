0b807b2afb899c98450a152b6cc65ba5367fd78c
deekshithreddy12 <deekshithch66@gmail.com>
2024-01-09 11:46:46 -0600

    last updated on tue morning

--- App.py
+++ App.py
@@ -1,40 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask , render_template , request , redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///storage.db"
 
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///storage.db"
+
+
+
+app.app_context()
+db =SQLAlchemy(app)
 
 class db_model(db.Model):
-    __tablename__ = 'db_model'
-    sno = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    desc = db.Column(db.String(500), nullable=False)
-    date_of_task = db.Column(db.DateTime, default=datetime.utcnow)
+    sno =db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(200),nullable=False)
+    desc=db.Column(db.String(500),nullable = False)
+    date_of_task =db.Column(db.DateTime, default =datetime.utcnow)
+
 
     def __repr__(self) -> str:
-        return f"{self.sno}-{self.title}"
+        return "{self.sno}-{self.title}"
 
-@app.route('/')
+
+@app.route('/',methods =['GET','POST'])
+
 def hello_world():
-    title = "Sample"
-    desc = "Computer is an electronic device"
-    task = db_model(title=title, desc=desc)
-    db.session.add(task)
-    db.session.commit()
+    if request.method =="POST":
+        title= request.form['title']
+        desc=request.form['desc']
+        
+        task =db_model(title = title, desc=desc)
+        db.session.add(task)
+        db.session.commit()
+    
+    altodo = db_model.query.all()
 
-    return render_template('index.html')
+
+    return render_template('index.html',altodo =altodo)
+
 
 @app.route('/about')
 def about():
-    return 'This is an about page'
+    return 'This is a about page'
 
-if __name__ == "__main__":
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    currenttodo = db_model.query.filter_by(sno=sno).first()
+    db.session.delete(currenttodo)
+    db.session.commit()
+
+    return redirect('/')
+
+
+
+if __name__ =="__main__":
     with app.app_context():
         db.create_all()
-    
-    app.run(debug=True)
+        app.run(debug=True)
