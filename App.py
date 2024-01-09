f1c7bbe05d42e29036aa8011360a049f55a19ca7
deekshithreddy12 <deekshithch66@gmail.com>
2024-01-08 18:25:01 -0600

    updated monday af

--- App.py
+++ App.py
@@ -1,14 +1,40 @@
-from flask import Flask , render_template
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
-if __name__=="__main__":
-    with app.app_context():
-        app.run(debug=True)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///storage.db"
+
+db = SQLAlchemy(app)
+
+class db_model(db.Model):
+    __tablename__ = 'db_model'
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_of_task = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno}-{self.title}"
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    title = "Sample"
+    desc = "Computer is an electronic device"
+    task = db_model(title=title, desc=desc)
+    db.session.add(task)
+    db.session.commit()
 
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return 'This is a about page'
+    return 'This is an about page'
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    
+    app.run(debug=True)
